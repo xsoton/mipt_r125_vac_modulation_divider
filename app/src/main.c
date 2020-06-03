fc0ec21300009f17007272e4e736fb850d82c3f1
Vladimir Gak <gak@phystech.edu>
2020-06-03 20:46:15 +0300

    Rewrite to USBTMC + GPIB

--- app/src/main.c
+++ app/src/main.c
@@ -13,48 +13,80 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <stdarg.h>
 
 #include <gpib/ib.h>
 
-// === config
-#define HANTEK_TMC "/dev/usbtmc0"
-#define PPS_GPIB_NAME "AKIP-1142/3G"
-#define VM_GPIB_NAME "AKIP-V7-78/1"
+// === config ===
 
-// === time
+#define OSC_NAME "/dev/usbtmc_dso4254c"
+
+#ifdef DEV_AKIP_TMC
+	#define PPS_NAME "/dev/usbtmc_1142_3g"
+	#define VM_NAME "/dev/usbtmc_v7_78_1"
+#else
+	#define PPS_NAME "AKIP-1142/3G"
+	#define VM_NAME "AKIP-V7-78/1"
+#endif
+
+// === time ===
+
 #define STEP_DELAY 1.0e6 // us
 
-// === pps
+// === pps ===
+
 #define VOLTAGE_MIN 0.0
 #define VOLTAGE_MAX 10.0
 #define VOLTAGE_STEP 0.1
 #define CURRENT_MAX 0.1
+#define LASER_VOLTAGE 5.0
+#define LASER_CURRENT 0.15
 
-// === threads
+// === threads ===
+
 void *commander(void *);
 void *worker(void *);
 
-// === utils
+// === utils ===
+
 int get_run();
 void set_run(int run_new);
+
 double get_time();
 
-int gpib_write(int fd, const char *str);
-int gpib_read(int fd, char *buf, long len);
-void gpib_print_error(int fd);
+int gpib_open(const char *name);
+int gpib_close(int dev);
+int gpib_write(int dev, const char *str);
+int gpib_read(int dev, char *buf, size_t buf_length);
+void gpib_print_error(int dev);
 
-int usbtmc_write(int dev, const char *cmd);
-int usbtmc_read(int dev, char *buf, int buf_length);
+int usbtmc_open(const char *name);
+int usbtmc_close(int dev);
+int usbtmc_write(int dev, const char *str);
+int usbtmc_read(int dev, char *buf, size_t buf_length);
 
+int dev_print(int dev, const char *format, ...);
 
-// === global variables
+#ifdef DEV_AKIP_TMC
+	#define dev_open  usbtmc_open
+	#define dev_close usbtmc_close
+	#define dev_read  usbtmc_read
+	#define dev_write usbtmc_write
+#else
+	#define dev_open  gpib_open
+	#define dev_close gpib_close
+	#define dev_read  gpib_read
+	#define dev_write gpib_write
+#endif
+
+// === global variables ===
 char dir_str[100];
 pthread_rwlock_t run_lock;
 int run;
 char filename_vac[100];
 const char *experiment_name;
 
-// === program entry point
+// === program entry point ===
 int main(int argc, char const *argv[])
 {
 	int ret = 0;
@@ -120,6 +152,8 @@ int main(int argc, char const *argv[])
 	// === and wait for cancelation finish
 	pthread_cancel(t_commander);
 	pthread_join(t_commander, NULL);
+
+	printf("\n");
 
 	main_exit:
 	return ret;
@@ -192,56 +226,42 @@ void *worker(void *arg)
 	FILE  *gp;
 	char   buf[100];
 
-	// === first we are connecting to instruments
-	r = open(HANTEK_TMC, O_RDWR);
-	if(r == -1)
-	{
-		fprintf(stderr, "# E: Unable to open hantek (%s)\n", strerror(errno));
-		goto worker_open_hantek;
-	}
+	// === first we have to connect to instruments
+	r = usbtmc_open(OSC_NAME);
+	if(r == -1) goto worker_osc_close;
 	osc_fd = r;
 
-	r = ibfind(PPS_GPIB_NAME);
-	if(r == -1)
-	{
-		fprintf(stderr, "# E: Unable to open power supply (%d)\n", r);
-		goto worker_pps_ibfind;
-	}
+	r = dev_open(PPS_NAME);
+	if(r == -1) goto worker_pps_open;
 	pps_fd = r;
 
-	r = ibfind(VM_GPIB_NAME);
-	if(r == -1)
-	{
-		fprintf(stderr, "# E: Unable to open voltmeter (%d)\n", r);
-		goto worker_vm_ibfind;
-	}
+	r = dev_open(VM_NAME);
+	if(r == -1) goto worker_vm_open;
 	vm_fd = r;
 
-#define DEBUG
-
 	// === init pps
-	gpib_write(pps_fd, "output 0");
-	gpib_write(pps_fd, "instrument:nselect 1");
-	gpib_write(pps_fd, "voltage:limit 11V");
-	gpib_write(pps_fd, "voltage 0.0");
-	gpib_write(pps_fd, "current 0.1");
-	gpib_write(pps_fd, "channel:output 1");
-	gpib_write(pps_fd, "instrument:nselect 2");
-	gpib_write(pps_fd, "voltage:limit 5.5V");
-	gpib_write(pps_fd, "voltage 5.0");
-	gpib_write(pps_fd, "current 0.15");
-	gpib_write(pps_fd, "channel:output 1");
-	gpib_write(pps_fd, "instrument:nselect 1");
+	dev_write(pps_fd, "output 0");
+	dev_write(pps_fd, "instrument:nselect 1");
+	dev_print(pps_fd, "voltage:limit %dV", ((int)VOLTAGE_MAX) + 1);
+	dev_write(pps_fd, "voltage 0.0");
+	dev_print(pps_fd, "current %.1lf", CURRENT_MAX);
+	dev_write(pps_fd, "channel:output 1");
+	dev_write(pps_fd, "instrument:nselect 2");
+	dev_print(pps_fd, "voltage:limit %.1lfV", LASER_VOLTAGE + 0.5);
+	dev_print(pps_fd, "voltage %.1lf", LASER_VOLTAGE);
+	dev_print(pps_fd, "current %.2lf", LASER_CURRENT);
+	dev_write(pps_fd, "channel:output 1");
+	dev_write(pps_fd, "instrument:nselect 1");
 	// gpib_print_error(pps_fd);
 
 	// === init vm
-	gpib_write(vm_fd, "function \"voltage:ac\"");
-	gpib_write(vm_fd, "voltage:ac:range:auto on");
-	gpib_write(vm_fd, "trigger:source immediate");
-	gpib_write(vm_fd, "trigger:delay:auto off");
-	gpib_write(vm_fd, "trigger:delay 0");
-	gpib_write(vm_fd, "trigger:count 1");
-	gpib_write(vm_fd, "sample:count 1");
+	dev_write(vm_fd, "function \"voltage:ac\"");
+	dev_write(vm_fd, "voltage:ac:range:auto on");
+	dev_write(vm_fd, "trigger:source immediate");
+	dev_write(vm_fd, "trigger:delay:auto off");
+	dev_write(vm_fd, "trigger:delay 0");
+	dev_write(vm_fd, "trigger:count 1");
+	dev_write(vm_fd, "sample:count 1");
 	// gpib_print_error(vm_fd);
 
 	// === init osc
@@ -259,33 +279,23 @@ void *worker(void *arg)
 	vac_fp = fopen(filename_vac, "w+");
 	if(vac_fp == NULL)
 	{
-		fprintf(stderr, "# E: Unable to open file \"%s\" (%s)\n", filename_vac, strerror(ferror(vac_fp)));
+		fprintf(stderr, "# E: unable to open file \"%s\" (%s)\n", filename_vac, strerror(errno));
 		goto worker_vac_fopen;
 	}
 	setlinebuf(vac_fp);
 
 	// === write vac header
-	r = fprintf(vac_fp,
-		"# mipt_r125_vac_modulation_divider\n"
-		"# Dependence of alternative voltage on voltage using resistive divider\n"
-		"# Experiment name \"%s\"\n"
-		"# Columns:\n"
-		"# 1 - index\n"
-		"# 2 - time, s\n"
-		"# 3 - pps voltage, V\n"
-		"# 4 - pps current, A\n"
-		"# 5 - vm voltage (ac), V\n"
-		"# 6 - laser voltage, V\n"
-		"# 7 - laser current, A\n"
-		"# 8 - laset modulation rate, Hz\n"
-		"# 9 - laset modulation duty, %%\n",
-		experiment_name
-	);
-	if(r < 0)
-	{
-		fprintf(stderr, "# E: Unable to print to file \"%s\" (%s)\n", filename_vac, strerror(r));
-		goto worker_vac_header;
-	}
+	fprintf(vac_fp, "# Dependence of alternative voltage on voltage using resistive divider\n");
+	fprintf(vac_fp, "# Experiment name \"%s\"\n", experiment_name);
+	fprintf(vac_fp, "# 1: index\n");
+	fprintf(vac_fp, "# 2: time, s\n");
+	fprintf(vac_fp, "# 3: pps voltage, V\n");
+	fprintf(vac_fp, "# 4: pps current, A\n");
+	fprintf(vac_fp, "# 5: vm current, A\n");
+	fprintf(vac_fp, "# 6: laser voltage, V\n");
+	fprintf(vac_fp, "# 7: laser current, A\n");
+	fprintf(vac_fp, "# 8: laset modulation rate, Hz\n");
+	fprintf(vac_fp, "# 9: laset modulation duty, %%\n");
 
 	// === open gnuplot
 	snprintf(buf, 100, "gnuplot > %s/gnuplot.log 2>&1", dir_str);
@@ -298,16 +308,9 @@ void *worker(void *arg)
 	setlinebuf(gp);
 
 	// === prepare gnuplot
-	r = fprintf(gp,
-		"set xrange [0:10]\n"
-		"set xlabel \"Voltage, V\"\n"
-		"set ylabel \"Voltage (AC), V\"\n"
-	);
-	if(r < 0)
-	{
-		fprintf(stderr, "# E: Unable to print to gp (%s)\n", strerror(r));
-		goto worker_gp_settings;
-	}
+	fprintf(gp, "set xrange [0:10]\n");
+	fprintf(gp, "set xlabel \"Voltage, V\"\n");
+	fprintf(gp, "set ylabel \"Voltage (AC), V\"\n");
 
 	// === let the action begins!
 	vac_index = 0;
@@ -321,83 +324,46 @@ void *worker(void *arg)
 			break;
 		}
 
-		snprintf(buf, 100, "voltage %.3lf", voltage);
-		gpib_write(pps_fd, buf);
+		dev_print(pps_fd, "voltage %.3lf", voltage);
 
 		usleep(STEP_DELAY);
 
 		vac_time = get_time();
-		if (vac_time < 0)
-		{
-			fprintf(stderr, "# E: Unable to get time\n");
-			set_run(0);
-			break;
-		}
 
-		gpib_write(pps_fd, "measure:voltage:all?");
-		gpib_read(pps_fd, buf, 100);
+		dev_write(pps_fd, "measure:voltage:all?");
+		dev_read(pps_fd, buf, 100);
 		sscanf(buf, "%lf, %lf", &pps_voltage, &laser_voltage);
-		// pps_voltage = atof(buf);
 
-		gpib_write(pps_fd, "measure:current:all?");
-		gpib_read(pps_fd, buf, 100);
+		dev_write(pps_fd, "measure:current:all?");
+		dev_read(pps_fd, buf, 100);
 		sscanf(buf, "%lf, %lf", &pps_current, &laser_current);
-		// pps_current = atof(buf);
 
-		gpib_write(vm_fd, "read?");
-		gpib_read(vm_fd, buf, 100);
+		dev_write(vm_fd, "read?");
+		dev_read(vm_fd, buf, 100);
 		vm_voltage = atof(buf);
 
-		r = fprintf(vac_fp, "%d\t%le\t%.3le\t%.3le\t%.8le\t%.3le\t%.3le\t%.1lf\t%d\n",
-			vac_index,
-			vac_time,
-			pps_voltage,
-			pps_current,
-			vm_voltage,
-			laser_voltage,
-			laser_current,
-			500.0,
-			50
+		fprintf(vac_fp, "%d\t%le\t%.3le\t%.3le\t%.8le\t%.3le\t%.3le\t%.1lf\t%d\n",
+			vac_index, vac_time, pps_voltage, pps_current, vm_voltage,
+			laser_voltage, laser_current, 500.0, 50
 		);
-		if(r < 0)
-		{
-			fprintf(stderr, "# E: Unable to print to file \"%s\" (%s)\n", filename_vac, strerror(r));
-			set_run(0);
-			break;
-		}
 
-		r = fprintf(gp,
+		fprintf(gp,
 			"set title \"i = %d, t = %.3lf s, Ul = %.3lf V, Il = %.3lf A, freq = %.1lf Hz, duty = %d %%\"\n"
 			"plot \"%s\" u 3:5 w l lw 1 title \"U = %.3lf V, Vac = %.3le V\"\n",
-			vac_index,
-			vac_time,
-			laser_voltage,
-			laser_current,
-			500.0,
-			50,
-			filename_vac,
-			pps_voltage,
-			vm_voltage
+			vac_index, vac_time, laser_voltage, laser_current, 500.0, 50,
+			filename_vac, pps_voltage, vm_voltage
 		);
-		if(r < 0)
-		{
-			fprintf(stderr, "# E: Unable to print to gp (%s)\n", strerror(r));
-			set_run(0);
-			break;
-		}
 
 		vac_index++;
 	}
 
-	gpib_write(pps_fd, "output 0");
-	gpib_write(pps_fd, "voltage 0");
+	dev_write(pps_fd, "output 0");
+	dev_write(pps_fd, "voltage 0");
 
 	usbtmc_write(osc_fd, "dds:switch 0");
 	usbtmc_write(osc_fd, "dds:offset 0");
 
-	gpib_write(pps_fd, "system:beeper");
-
-	worker_gp_settings:
+	dev_write(pps_fd, "system:beeper");
 
 	r = pclose(gp);
 	if (r == -1)
@@ -406,9 +372,6 @@ void *worker(void *arg)
 	}
 	worker_gp_popen:
 
-
-	worker_vac_header:
-
 	r = fclose(vac_fp);
 	if (r == EOF)
 	{
@@ -416,29 +379,20 @@ void *worker(void *arg)
 	}
 	worker_vac_fopen:
 
-	ibclr(vm_fd);
-	gpib_write(vm_fd, "*rst");
-	sleep(1);
-	ibloc(vm_fd);
-	worker_vm_ibfind:
+	dev_close(vm_fd);
+	worker_vm_open:
 
-	ibclr(pps_fd);
-	gpib_write(pps_fd, "*rst");
-	sleep(1);
-	ibloc(pps_fd);
-	worker_pps_ibfind:
+	dev_close(pps_fd);
+	worker_pps_open:
 
-	r = close(osc_fd);
-	if(r == -1)
-	{
-		fprintf(stderr, "# E: Unable to close hantek (%s)\n", strerror(errno));
-	}
-	worker_open_hantek:
+	usbtmc_close(osc_fd);
+	worker_osc_close:
 
 	return NULL;
 }
 
-// === utils
+// === UTILS ===
+
 int get_run()
 {
 	int run_local;
@@ -495,57 +449,174 @@ double get_time()
 	return ret;
 }
 
+// === GPIB ===
 
-int gpib_write(int fd, const char *str)
-{
-	return ibwrt(fd, str, strlen(str));
-}
-
-int gpib_read(int fd, char *buf, long len)
+int gpib_open(const char *name)
 {
 	int r;
 
-	r = ibrd(fd, buf, len);
-	if (ibcnt < len)
-	{
-		buf[ibcnt] = 0;
-	}
-
-	return r;
-}
-
-void gpib_print_error(int fd)
-{
-#ifdef DEBUG
-	char buf[100] = {0};
-	gpib_write(fd, "system:error?");
-	gpib_read(fd, buf, 100);
-	fprintf(stderr, "[debug] error = %s\n", buf);
-#endif
-}
-
-int usbtmc_write(int dev, const char *cmd)
-{
-	int r;
-
-	r = write(dev, cmd, strlen(cmd));
+	r = ibfind(name);
 	if (r == -1)
 	{
-		fprintf(stderr, "# E: unable to write to hantek (%s)\n", strerror(errno));
+		fprintf(stderr, "# E: unable to open gpib (ibsta = %d, iberr = %d)\n", ibsta, iberr);
 	}
 
 	return r;
 }
 
-int usbtmc_read(int dev, char *buf, int buf_length)
+int gpib_close(int dev)
+{
+	int r;
+	int ret = 0;
+
+	r = ibclr(dev);
+	if (r & 0x8000)
+	{
+		ret = -1;
+		fprintf(stderr, "# E: unable to clr gpib (ibsta = %d, iberr = %d)\n", ibsta, iberr);
+	}
+
+	r = gpib_write(dev, "*rst");
+	if (r == -1)
+	{
+		ret = r;
+	}
+
+	sleep(1);
+
+	r = ibloc(dev);
+	if (r & 0x8000)
+	{
+		ret = -1;
+		fprintf(stderr, "# E: unable to loc gpib (ibsta = %d, iberr = %d)\n", ibsta, iberr);
+	}
+
+	return ret;
+}
+
+int gpib_write(int dev, const char *str)
+{
+	int r;
+	int ret = 0;
+
+	r = ibwrt(dev, str, strlen(str));
+	if (r & 0x8000)
+	{
+		ret = -1;
+		fprintf(stderr, "# E: unable to write to gpib (ibsta = %d, iberr = %d)\n", ibsta, iberr);
+	}
+	else
+	{
+		ret = ibcnt;
+	}
+
+	return ret;
+}
+
+int gpib_read(int dev, char *buf, size_t buf_length)
+{
+	int r;
+	int ret = 0;
+
+	r = ibrd(dev, buf, buf_length);
+	if (r & 0x8000)
+	{
+		ret = -1;
+		fprintf(stderr, "# E: unable to write to gpib (ibsta = %d, iberr = %d)\n", ibsta, iberr);
+	}
+	else
+	{
+		ret = ibcnt;
+		if (ibcnt < buf_length)
+		{
+			buf[ibcnt] = 0;
+		}
+	}
+
+	return ret;
+}
+
+void gpib_print_error(int dev)
+{
+	char buf[100] = {0};
+	gpib_write(dev, "system:error?");
+	gpib_read(dev, buf, 100);
+	fprintf(stderr, "# [debug] error = %s\n", buf);
+}
+
+// === USBTMC ===
+
+int usbtmc_open(const char *name)
+{
+	int r;
+
+	r = open(name, O_RDWR);
+	if (r == -1)
+	{
+		fprintf(stderr, "# E: unable to open usbtmc \"%s\" (%s)\n", name, strerror(errno));
+	}
+
+	return r;
+}
+
+int usbtmc_close(int dev)
+{
+	int r;
+
+	usbtmc_write(dev, "*rst");
+
+	r = close(dev);
+	if (r == -1)
+	{
+		fprintf(stderr, "# E: unable to close usbtmc (%s)\n", strerror(errno));
+	}
+
+	return r;
+}
+
+int usbtmc_write(int dev, const char *str)
+{
+	int r;
+
+	r = write(dev, str, strlen(str));
+	if (r == -1)
+	{
+		fprintf(stderr, "# E: unable to write to usbtmc \"%s\" (%s)\n", str, strerror(errno));
+	}
+
+	return r;
+}
+
+int usbtmc_read(int dev, char *buf, size_t buf_length)
 {
 	int r;
 
 	r = read(dev, buf, buf_length);
 	if (r == -1)
 	{
-		fprintf(stderr, "# E: unable to read from hantek (%s)\n", strerror(errno));
+		fprintf(stderr, "# E: unable to read from usbtmc (%s)\n", strerror(errno));
 	}
+
+	return r;
+}
+
+int dev_print(int dev, const char *format, ...)
+{
+	int r;
+	va_list args;
+	char buf[100];
+	const size_t bufsize = 100;
+
+	va_start(args, format);
+	r = vsnprintf(buf, bufsize, format, args);
+	if (r < 0)
+	{
+		fprintf(stderr, "# E: unable to printf to buffer (%s)\n", strerror(errno));
+		goto usbtmc_print_vsnprintf;
+	}
+	r = dev_write(dev, buf);
+	usbtmc_print_vsnprintf:
+	va_end(args);
 
 	return r;
 }
